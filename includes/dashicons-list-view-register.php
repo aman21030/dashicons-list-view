8a550daaa5b2732eecd0280019fa53f0e5e114cd
to-aman <i10v3.3.6bc@gmail.com>
2022-08-27 15:27:55 +0900

    8/27の作業分

--- includes/dashicons-list-view-register.php
+++ includes/dashicons-list-view-register.php
@@ -1,14 +1,30 @@
 <?php
 class Dashicons_List_View_Register {
+    private $option = 'dashicons_list_view';
     public function __construct() {
+        $this->update();
         $this->page_render();
     }
 
+    private function update() {
+        if ( isset ( $_POST['submit'] ) && $_POST['submit'] === 'update' ) {
+            if ( ! isset( $_POST['list_view_field'] ) || ! wp_verify_nonce( $_POST['list_view_field'], 'list_view_action' ) ) {
+            //    print 'Sorry, your nonce did not verify.';
+               exit;
+            } else {  
+                update_option( $this->option, $_POST );
+            }  
+        }
+    }
+
     private function page_render() {
+        $args = get_option( $this->option );
         $html  = '<div class="wrap">';
         $html .= '<h1>Dashicons List View Register</h1>';
         $html .= '<form method="POST" action="">';
-        $html .= '<table>';
+        echo $html;
+        wp_nonce_field( 'list_view_action', 'list_view_field' );
+        $html  = '<table>';
         $html .= '<tr>';
         $html .= '<th>&nbsp;</th>';
         $html .= '<th>Account</th>';
@@ -17,7 +33,7 @@ class Dashicons_List_View_Register {
         $html .= '</tr>';
         $html .= '<tr>';
         $html .= '<th><label for="twitter_account">Twitter</label></th>';
-        $html .= '<td><input type="text" name="twitter_account" id="twitter_account" maxlength="30"></td>';
+        $html .= '<td><input type="text" name="twitter_account" id="twitter_account" maxlength="30" value="' . $args['twitter_account'] . '"></td>';
         $html .= '<td>W:<input type="number" name="twitter_size_w" maxlength="3">H:<input type="number" name="twitter_size_h" maxlength="3"></td>';
         $html .= '<td><input type="text" name="twitter_color" id="twitter-color-picker"></td>';
         $html .= '</tr>';
@@ -46,6 +62,7 @@ class Dashicons_List_View_Register {
         $html .= '<td><input type="text" name="pinterest_color" id="pinterest-color-picker"></td>';
         $html .= '</tr>';
         $html .= '</table>';
+        $html .= '<input type="submit" value="update" name="submit">';
         $html .= '</form>';
         $html .= '</div>';
         echo $html;
