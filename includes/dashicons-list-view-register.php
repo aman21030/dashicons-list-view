921d3ba9c8192887b082d12583f22e7f43253dd3
to-aman <i10v3.3.6bc@gmail.com>
2022-09-17 15:31:34 +0900

    9/17の作業分

--- includes/dashicons-list-view-register.php
+++ includes/dashicons-list-view-register.php
@@ -1,6 +1,7 @@
 <?php
 class Dashicons_List_View_Register {
     private $option = 'dashicons_list_view';
+    private $update_flag = false;
     public function __construct() {
         $this->update();
         $this->page_render();
@@ -12,16 +13,49 @@ class Dashicons_List_View_Register {
             //    print 'Sorry, your nonce did not verify.';
                exit;
             } else {  
-                update_option( $this->option, $_POST );
+                $this->update_flag = update_option( $this->option, $_POST );
             }  
         }
     }
 
+    private function init_data() {
+        $args['twitter_account']   = '';
+        $args['twitter_size_w']    = '';
+        $args['twitter_size_h']    = '';
+        $args['twitter_color']     = '';
+        $args['facebook_account']  = '';
+        $args['facebook_size_w']   = '';
+        $args['facebook_size_h']   = '';
+        $args['facebook_color']    = '';
+        $args['instagram_account'] = '';
+        $args['instagram_size_w']  = '';
+        $args['instagram_size_h']  = '';
+        $args['instagram_color']   = '';
+        $args['youtube_account']   = '';
+        $args['youtube_size_w']    = '';
+        $args['youtube_size_h']    = '';
+        $args['youtube_color']     = '';
+        $args['pinterest_account'] = '';
+        $args['pinterest_size_w']  = '';
+        $args['pinterest_size_h']  = '';
+        $args['pinterest_color']   = '';
+        return $args;
+    }
+
     private function page_render() {
         $args = get_option( $this->option );
+        if ( !$args ) {
+            $args = $this->init_data();
+        }
+
         $html  = '<div class="wrap">';
         $html .= '<h1>Dashicons List View Register</h1>';
-        $html .= '<form method="POST" action="">';
+        echo $html;
+        if ( $this->update_flag ) {
+            $this->information_render();
+            $this->update_flag = false;
+        }
+        $html  = '<form method="POST" action="">';
         echo $html;
         wp_nonce_field( 'list_view_action', 'list_view_field' );
         $html  = '<table>';
@@ -34,32 +68,37 @@ class Dashicons_List_View_Register {
         $html .= '<tr>';
         $html .= '<th><label for="twitter_account">Twitter</label></th>';
         $html .= '<td><input type="text" name="twitter_account" id="twitter_account" maxlength="30" value="' . $args['twitter_account'] . '"></td>';
-        $html .= '<td>W:<input type="number" name="twitter_size_w" maxlength="3">H:<input type="number" name="twitter_size_h" maxlength="3"></td>';
-        $html .= '<td><input type="text" name="twitter_color" id="twitter-color-picker"></td>';
+        $html .= '<td>W:<input type="number" name="twitter_size_w" maxlength="3" value="' . $args['twitter_size_w'] . '">';
+        $html .= 'H:<input type="number" name="twitter_size_h" maxlength="3" value="' . $args['twitter_size_h'] . '"></td>';
+        $html .= '<td><input type="text" name="twitter_color" id="twitter-color-picker" value="' . $args['twitter_color'] . '"></td>';
         $html .= '</tr>';
         $html .= '<tr>';
         $html .= '<th><label for="facebook_account">Facebook</label></th>';
-        $html .= '<td><input type="text" name="facebook_account" id="facebook_account" maxlength="30"></td>';
-        $html .= '<td>W:<input type="number" name="facebook_size_w" maxlength="3">H:<input type="number" name="facebook_size_h" maxlength="3"></td>';
-        $html .= '<td><input type="text" name="facebook_color" id="facebook-color-picker"></td>';
+        $html .= '<td><input type="text" name="facebook_account" id="facebook_account" maxlength="30" value="' . $args['facebook_account'] . '"></td>';
+        $html .= '<td>W:<input type="number" name="facebook_size_w" maxlength="3" value="' . $args['facebook_size_w'] . '">';
+        $html .= 'H:<input type="number" name="facebook_size_h" maxlength="3" value="' . $args['facebook_size_h'] . '"></td>';
+        $html .= '<td><input type="text" name="facebook_color" id="facebook-color-picker" value="' . $args['facebook_color'] . '"></td>';
         $html .= '</tr>';
         $html .= '<tr>';
         $html .= '<th><label for="instagram_account">Instagram</label></th>';
-        $html .= '<td><input type="text" name="instagram_account" id="instagram_account" maxlength="30"></td>';
-        $html .= '<td>W:<input type="number" name="instagram_size_w" maxlength="3">H:<input type="number" name="instagram_size_h" maxlength="3"></td>';
-        $html .= '<td><input type="text" name="instagram_color" id="instagram-color-picker"></td>';
+        $html .= '<td><input type="text" name="instagram_account" id="instagram_account" maxlength="30" value="' . $args['instagram_account'] . '"></td>';
+        $html .= '<td>W:<input type="number" name="instagram_size_w" maxlength="3" value="' . $args['instagram_size_w'] . '">';
+        $html .= 'H:<input type="number" name="instagram_size_h" maxlength="3" value="' . $args['instagram_size_h'] . '"></td>';
+        $html .= '<td><input type="text" name="instagram_color" id="instagram-color-picker" value="' . $args['instagram_color'] . '"></td>';
         $html .= '</tr>';
         $html .= '<tr>';
         $html .= '<th><label for="youtube_account">YouTube</label></th>';
-        $html .= '<td><input type="text" name="youtube_account" id="youtube_account" maxlength="30"></td>';
-        $html .= '<td>W:<input type="number" name="youtube_size_w" maxlength="3">H:<input type="number" name="youtube_size_h" maxlength="3"></td>';
-        $html .= '<td><input type="text" name="youtube_color" id="youtube-color-picker"></td>';
+        $html .= '<td><input type="text" name="youtube_account" id="youtube_account" maxlength="30" value="' . $args['youtube_account'] . '"></td>';
+        $html .= '<td>W:<input type="number" name="youtube_size_w" maxlength="3" value="' . $args['youtube_size_w'] . '">';
+        $html .= 'H:<input type="number" name="youtube_size_h" maxlength="3" value="' . $args['youtube_size_h'] . '"></td>';
+        $html .= '<td><input type="text" name="youtube_color" id="youtube-color-picker" value="' . $args['youtube_color'] . '"></td>';
         $html .= '</tr>';
         $html .= '<tr>';
         $html .= '<th><label for="pinterest_account">Pinterest</label></th>';
-        $html .= '<td><input type="text" name="pinterest_account" id="pinterest_account" maxlength="30"></td>';
-        $html .= '<td>W:<input type="number" name="pinterest_size_w" maxlength="3">H:<input type="number" name="pinterest_size_h" maxlength="3"></td>';
-        $html .= '<td><input type="text" name="pinterest_color" id="pinterest-color-picker"></td>';
+        $html .= '<td><input type="text" name="pinterest_account" id="pinterest_account" maxlength="30" value="' . $args['pinterest_account'] . '"></td>';
+        $html .= '<td>W:<input type="number" name="pinterest_size_w" maxlength="3" value="' . $args['pinterest_size_w'] . '">';
+        $html .= 'H:<input type="number" name="pinterest_size_h" maxlength="3" value="' . $args['pinterest_size_h'] . '"></td>';
+        $html .= '<td><input type="text" name="pinterest_color" id="pinterest-color-picker" value="' . $args['pinterest_color'] . '"></td>';
         $html .= '</tr>';
         $html .= '</table>';
         $html .= '<input type="submit" value="update" name="submit">';
@@ -67,4 +106,14 @@ class Dashicons_List_View_Register {
         $html .= '</div>';
         echo $html;
     }
+    private function information_render () {
+		$html  = '<div id="message" class="updated notice notice-success is-dismissible below-h2">';
+		$html .= '<p>Information Update.</p>';
+		$html .= '<button type="button" class="notice-dismiss">';
+		$html .= '<span class="screen-reader-text">Information Update.</span>';
+		$html .= '</button>';
+		$html .= '</div>';
+
+		echo $html;
+	}
 }
